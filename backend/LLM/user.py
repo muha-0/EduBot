dc91021935c56079bc8f801d4b00c7bdda84940f
Shady Ali <121682078+SHIXOOM@users.noreply.github.com>
2024-12-11 17:56:16 +0200

    Optimizing Code + Bug Fixing

--- backend/LLM/user.py
+++ backend/LLM/user.py
@@ -18,61 +18,32 @@ class User:
         self.physical_activity = None
         self.tutoring = None
 
-    def update(self, **kwargs):
+    def update(self, attributes):
         """Update the user's attributes
             kwargs should have a JSON object with each attribute and its extracted value:
             -If the value is an empty string, then the LLM did not extract any value for it and we want to keep it as None.
             -Else, we assign the extracted value to its attribute.
         """
-        for attribute, value in kwargs.items():
-            if (value != ''):
-                if attribute.__contains__("name"):
-                    self.name = value
-
-                elif attribute.__contains__("age"):
-                    self.age = value
-
-                elif attribute.__contains__("gender"):
-                    self.gender = value
-
-                elif attribute.__contains__("study"):
-                    self.study_hours_per_week = value
-
-                elif attribute.__contains__("sleep"):
-                    self.sleep_hours_per_night = value
-
-                elif attribute.__contains__("exam"):
-                    self.previous_exam_scores = value
-
-                elif attribute.__contains__("motivation"):
-                    self.motivation_level = value
-
-                elif attribute.__contains__("attendance"):
-                    self.class_attendance = value
-
-                elif attribute.__contains__("teacher"):
-                    self.teacher_quality = value
-
-                elif attribute.__contains__("resource"):
-                    self.resource_access = value
-
-                elif attribute.__contains__("extracurricular"):
-                    self.extracurricular_activities = value
-
-                elif attribute.__contains__("school"):
-                    self.school_type = value
-
-                elif attribute.__contains__("peer") or attribute.__contains__("influence"):
-                    self.peer_influence = value
-
-                elif attribute.__contains__("learning") or attribute.__contains__("disabilities"):
-                    self.learning_disabilities = value
-
-                elif attribute.__contains__("distance") or attribute.__contains__("home"):
-                    self.distance_from_home = value
-
-                elif attribute.__contains__("physical"):
-                    self.physical_activity = value
+    
+        self.name = attributes.name
+        self.age = attributes.age
+        self.gender = attributes.gender
+        self.study_hours_per_week = attributes.study_hours_per_week
+        self.sleep_hours_per_night = attributes.sleep_hours_per_night
+        self.previous_exam_scores = attributes.previous_exam_scores
+        self.motivation_level = attributes.motivation_level
+        self.class_attendance = attributes.class_attendance
+        self.teacher_quality = attributes.teacher_quality
+        self.resource_access = attributes.resource_access
+        self.extracurricular_activities = attributes.extracurricular_activities
+        self.school_type = attributes.school_type
+        self.peer_influence = attributes.peer_influence
+        self.learning_disabilities = attributes.learning_disabilities
+        self.distance_from_home = attributes.distance_from_home
+        self.physical_activity = attributes.physical_activity
+        self.tutoring = attributes.tutoring
+        
+        print("\n\nupdated user attributes: ", self.data())
 
 
     # TODO: Add tutoring attribute 
